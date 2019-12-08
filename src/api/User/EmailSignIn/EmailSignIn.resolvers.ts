da7d27d3f73fdd7f6156966a05321ab9689d73b6
gonewbie <gounewbie@gmail.com>
2019-12-08 21:49:53 +0900

    add: create EmailSignIn type, mutation, resolvers

---
+++ src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -0,0 +1,43 @@
+import { EmailSignInResponse, EmailSignInMutationArgs } from '../../../types/graph';
+import { Resolvers } from '../../../types/resolvers';
+import User from '../../../entities/User';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignIn: async (_, args: EmailSignInMutationArgs): Promise<EmailSignInResponse> => {
+      try {
+        const { email, password } = args;
+        const user = await User.findOne({ email });
+        if (!user) {
+          return {
+            ok: false,
+            error: 'No User found with that email',
+            token: null
+          }
+        }
+        const checkPassword = await user.comparePassword(password);
+        if (checkPassword) {
+          return {
+            ok: true,
+            error: null,
+            token: 'Comming Soon'
+          }
+        } else {
+          return {
+            ok: false,
+            error: 'Wrong password',
+            token: null
+          }
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        }
+      }
+    }
+  }
+}
+
+export default resolvers;
