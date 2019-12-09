4dff9b7f7650a2bf41ebbe2b669fc7c495d7fa6d
gonewbie <gounewbie@gmail.com>
2019-12-09 19:24:36 +0900

    add: createJWT logics

---
+++ src/utils/createJWT.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+
+const createJWT = (id: number): string => {
+  const token = jwt.sign(
+  {
+    id
+  }, process.env.JWT_TOKEN || ''
+  );
+  return token;
+}
+
+export default createJWT;
