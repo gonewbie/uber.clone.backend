cdba9d32257809ea3a6252d09c9ad55edec48f77
gonewbie <gounewbie@gmail.com>
2019-12-09 20:45:25 +0900

    add: pass user object through graphQL resolver

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,14 @@ class App {
   public app: GraphQLServer;
 
   constructor() {
-    this.app = new GraphQLServer({schema});
+    this.app = new GraphQLServer({
+      schema,
+      context: req => {
+        return {
+          req: req.request
+        }
+      }
+    });
     this.middlewares();
   }
 
@@ -24,7 +31,7 @@ class App {
     const token = req.get('X-JWT');
     if (token) {
       const user = await decodeJWT(token);
-      console.log(user);
+      req.user = user;
     }
     next();
   }
