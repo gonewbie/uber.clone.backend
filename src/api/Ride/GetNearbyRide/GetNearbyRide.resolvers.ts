f0f21220d7f06fe748dd55cac48e17c0779e741a
gonewbie <gounewbie@gmail.com>
2019-12-13 23:29:23 +0900

    add: create GetNearbyRide type, query, resolvers

---
+++ src/api/Ride/GetNearbyRide/GetNearbyRide.resolvers.ts
@@ -0,0 +1,53 @@
+import { Between, getRepository } from "typeorm";
+import Ride from "../../../entities/Ride";
+import User from "../../../entities/User";
+import { GetNearbyRideResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetNearbyRide: privateResolver(
+      async (_, __, { req }): Promise<GetNearbyRideResponse> => {
+        const user: User = req.user;
+        if (user.isDriving) {
+          const { lastLat, lastLng } = user;
+          try {
+            const ride = await getRepository(Ride).findOne({
+              status: 'REQUESTING',
+              pickUpLat: Between(lastLat - 0.05, lastLat + 0.05),
+              pickUpLng: Between(lastLng - 0.05, lastLng + 0.05)
+            });
+            if (ride) {
+              return {
+                ok: true,
+                error: null,
+                ride
+              };
+            } else {
+              return {
+                ok: true,
+                error: null,
+                ride: null
+              };
+            }
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+              ride: null
+            };
+          } 
+        } else {
+          return {
+            ok: false,
+            error: 'You are not a driver',
+            ride: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
