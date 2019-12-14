4dd744663987873d38520f8d260f1b2859a62152
gonewbie <gounewbie@gmail.com>
2019-12-15 02:02:54 +0900

    add: GetRide type, query, resolvers

--- src/entities/Ride.ts
+++ src/entities/Ride.ts
@@ -11,7 +11,7 @@ class Ride extends BaseEntity {
   @Column({
     type: "text",
     enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"],
-    default: "ACCEPTED"
+    default: "REQUESTING"
   })
   status: rideStatus;
 
@@ -42,17 +42,17 @@ class Ride extends BaseEntity {
   @Column({ type: "text" })
   duration: string;
 
+  @Column({ nullable: true })
+  passengerId: number;
+
   @ManyToOne(type => User, user => user.ridesAsPassenger)
   passenger: User;
 
   @Column({ nullable: true })
-  passengerId: number;
+  driverId: number;
 
   @ManyToOne(type => User, user => user.ridesAsDriver, { nullable: true })
   driver: User;
-
-  @Column({ nullable: true })
-  driverId: number;
 
   @CreateDateColumn() createdAt: string;
 
